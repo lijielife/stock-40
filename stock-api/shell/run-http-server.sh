df60f9c89504fa8835383911f966da0b9b02491d
chenlisong <chenlisong@alibaba-inc.com>
2017-01-03 19:04:43 +0800

    修改脚本java内存

--- stock-api/shell/run-http-server.sh
+++ stock-api/shell/run-http-server.sh
@@ -6,7 +6,7 @@ MODE=http-server
 
 source $BIN_PATH/config.sh
 
-JVMOPTS="-server -Xms1g -Xmx2g -XX:NewSize=128m -Xss512k -XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
+JVMOPTS="-server -Xms1g -Xmx2g -XX:NewSize=512m -Xss512k -XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
 JVMARGS="-Dmode=$MODE -Dserver.port=$HTTP_PORT"
 
 source $BIN_PATH/common.sh
